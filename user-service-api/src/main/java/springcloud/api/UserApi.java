4749ff04c2444d0aba77bf1717bfaec5e64ba339
xiaofengczy <1183676409@qq.com>
2019-08-06 17:35:08 +0800

    修改api

--- user-service-api/src/main/java/springcloud/api/UserApi.java
+++ user-service-api/src/main/java/springcloud/api/UserApi.java
@@ -1,11 +1,12 @@
 package springcloud.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import springcloud.entity.Params;
 import springcloud.entity.User;
-
 /**
  * FileName: UserApi Description:
  *
@@ -18,5 +19,8 @@ public interface UserApi {
 
   @GetMapping("/user/{id}")
   User findUserById(@PathVariable("id") String userId);
+
+  @GetMapping("/demo")
+  String demoEndPoint(@SpringQueryMap Params params);
 
 }
---
+++ user-service-api/src/main/java/springcloud/entity/Params.java
@@ -0,0 +1,17 @@
+package springcloud.entity;
+
+import lombok.Data;
+
+/**
+ * FileName: Params Description:
+ *
+ * @author caozhongyu
+ * @create 19-8-6
+ */
+@Data
+public class Params {
+
+  private String param1;
+  private String param2;
+
+}
